d47ef8624bb3d94eb2a4fa88ee7a7d620e96b25a
Alexandru Adam <dm_alexandru@yahoo.com>
2020-07-30 15:21:17 +0300

    Started working on a function to calculate the risk of ruin

--- roulette.py
+++ roulette.py
@@ -8,9 +8,9 @@ from tqdm import trange
 DEPOSIT = 1000
 BET = 80
 TARGET = 3000
-NUM_SIMS = 100000
+NUM_SIMS = 1000000
 CASHBACK = 0.07
-
+BANKROLL = 100000
 
 def roulette_bet(deposit,bet,target):
 	balance = deposit
@@ -37,11 +37,21 @@ def simulate(deposit, bet, target,num_sims, cashback):
 			results.append(result)
 	return results
 
+def calculate_risk_of_ruin(results, bank):
+	bankruptcies =0
+	for result in results:
+		bank +=result
+		if bank < 0:
+			bankruptcies +=1
+			bank = BANKROLL
+	print(f'You lost your bankroll {bankruptcies} times in {len(results)} deposits')
+	return bankruptcies
 
 def main():
 	simulation = np.array(simulate(DEPOSIT, BET, TARGET, NUM_SIMS, CASHBACK))
 	print('EV for this simulation is: {:.2f}'.format(simulation.mean()))
 	#roulette_bet(DEPOSIT, BET, TARGET)
+	calculate_risk_of_ruin(simulation, BANKROLL)
 
 if __name__ == '__main__':
 	main()
