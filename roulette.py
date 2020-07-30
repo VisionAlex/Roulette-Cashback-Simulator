595e2c53bc4e714e4ce581d678b17974e3ca2cd7
Alexandru Adam <dm_alexandru@yahoo.com>
2020-07-30 14:44:59 +0300

    Fixed a bug  when setting a bet not divisable by the target Now the roulette script reduces the bet when it is less than the balance and raises it back if enough  balance.

--- roulette.py
+++ roulette.py
@@ -6,20 +6,24 @@ from tqdm import trange
 
 
 DEPOSIT = 1000
-BET = 100
+BET = 80
 TARGET = 3000
-NUM_SIMS = 1000000
+NUM_SIMS = 100000
 CASHBACK = 0.07
 
 
 def roulette_bet(deposit,bet,target):
-	outcomes =[-bet, bet*35]
 	balance = deposit
-	while (bet <= balance) and (balance <target) and balance >0:
-		balance += random.choices(outcomes, weights=(36,1))[0]
+	def check(bet):
 		if bet > balance:
 			bet = balance
-		#print(balance)
+		if bet < balance and BET <= balance:
+			bet = BET
+		return bet
+	while bet <= balance and balance < target and balance > 0:
+		bet =check(bet)
+		balance += random.choices([-bet, bet*35], weights=(36,1))[0]
+		bet = check(bet)
 	return balance -deposit
 
 
@@ -37,6 +41,7 @@ def simulate(deposit, bet, target,num_sims, cashback):
 def main():
 	simulation = np.array(simulate(DEPOSIT, BET, TARGET, NUM_SIMS, CASHBACK))
 	print(f'EV for this simulation is: {simulation.mean()}')
+	#roulette_bet(DEPOSIT, BET, TARGET)
 
 if __name__ == '__main__':
 	main()
